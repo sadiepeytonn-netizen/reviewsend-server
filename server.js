1d6903d21db2b552fc21e369e63aa516559674bb
sadiepeytonn-netizen <sadiepeytonn@gmail.com>
2026-04-16 20:00:47 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const twilio = require("twilio");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const accountSid = "AC96d85180675dcb1fff96bebc096b4868";
+const authToken = "30d44e1b9052e2b8107210f2e674eda5";
+const twilioPhone = "+18334395495";
+
+const client = twilio(accountSid, authToken);
+
+app.post("/send-sms", async (req, res) => {
+  const { to, message } = req.body;
+
+  try {
+    const result = await client.messages.create({
+      body: message,
+      from: twilioPhone,
+      to: to,
+    });
+    res.json({ success: true, sid: result.sid });
+  } catch (error) {
+    res.json({ success: false, error: error.message });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("ReviewSend server is running!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
